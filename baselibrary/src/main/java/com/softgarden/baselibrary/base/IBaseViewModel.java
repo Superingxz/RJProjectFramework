5bea915017fcb90c2a162449d3660c2d6e922c9f
superingxz <1803117759@qq.com>
2017-11-03 11:09:32 +0800

    1.improve codes

--- baselibrary/src/main/java/com/softgarden/baselibrary/base/IBaseViewModel.java
+++ baselibrary/src/main/java/com/softgarden/baselibrary/base/IBaseViewModel.java
@@ -3,8 +3,8 @@ package com.softgarden.baselibrary.base;
 /**
  * Created by Administrator on 2016/9/10 0010.
  */
-public interface IBaseViewModel<T extends IBaseDisplay> {
-    void attachView(T view);
+public interface IBaseViewModel<V extends IBaseDisplay> {
+    void attachView(V view);
 
     void detachView();
 }
--- baselibrary/src/main/java/com/softgarden/baselibrary/base/RxViewModel.java
+++ baselibrary/src/main/java/com/softgarden/baselibrary/base/RxViewModel.java
@@ -4,11 +4,11 @@ package com.softgarden.baselibrary.base;
 /**
  * 引入 IBaseDisplay mView  控制RxJava生命周期
  */
-public class RxViewModel<T extends IBaseDisplay> implements IBaseViewModel<T> {
-    protected T mView;
+public class RxViewModel<V extends IBaseDisplay> implements IBaseViewModel<V> {
+    protected V mView;
 
     @Override
-    public void attachView(T display) {
+    public void attachView(V display) {
         this.mView = display;
     }
 
